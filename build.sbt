31c9cbabaaafe28c4446df9abe4fda49b3fa98b0
mwielocha <mwielocha@icloud.com>
2017-06-21 19:13:53 +0200

    basic project config part II

--- build.sbt
+++ build.sbt
@@ -10,14 +10,16 @@ lazy val root = (project in file(".")).
 
 mainClass in (Compile, run) := Some("io.cyberdolphin.biketracker.StartServer")
 
-val akkaVersion = "2.5.3"
 val akkaHttpVersion = "10.0.8"
+val circeVersion = "0.8.0"
 
 libraryDependencies ++= Seq(
-  "com.typesafe.akka" %% "akka-actor" % akkaVersion,
   "com.typesafe.akka" %% "akka-http" % akkaHttpVersion,
+  "de.heikoseeberger" %% "akka-http-circe" % "1.17.0",
+  "io.circe" %% "circe-core" % circeVersion,
+  "io.circe" %% "circe-generic" % circeVersion,
+  "io.circe" %% "circe-parser" % circeVersion,
   "com.typesafe.akka" %% "akka-http-testkit" % akkaHttpVersion % Test,
-  "com.typesafe.akka" %% "akka-testkit" % akkaVersion % Test,
   "org.scalatest" %% "scalatest" % "3.0.0" % Test
 )
 
--- src/main/scala/io/cyberdolphin/biketracker/StartServer.scala
+++ src/main/scala/io/cyberdolphin/biketracker/StartServer.scala
@@ -1,12 +1,37 @@
 package io.cyberdolphin.biketracker
 
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.server.Directives
+import akka.stream.{ ActorMaterializer, Materializer }
+import scala.concurrent.Future
+import de.heikoseeberger.akkahttpcirce.FailFastCirceSupport
+
+
 /**
  * Created by Mikolaj Wielocha on 21/06/17
  */
 
 object StartServer extends App {
 
+  implicit val system = ActorSystem()
+  implicit val mat = ActorMaterializer()
 
-  println("Hi")
+  Http().bindAndHandle(route, "127.0.0.1", 9021)
 
+  scala.sys.addShutdownHook {
+    system.terminate()
+  }
+
+  private def route(implicit mat: Materializer) = {
+    import Directives._
+    import FailFastCirceSupport._
+    import io.circe.generic.auto._
+
+    pathPrefix("api") {
+      (path("config") & get) {
+        complete { Future.successful("Hi") }
+      }
+    }
+  }
 }
